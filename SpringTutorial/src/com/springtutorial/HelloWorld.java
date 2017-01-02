747e350aaa2358a8896b5c3d8f7d0985fa427233
Ashutosh <sharashu@amazon.com>
2017-01-02 22:10:35 +0530

    Adding SpringTutorial

---
+++ SpringTutorial/src/com/springtutorial/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.springtutorial;
+
+public class HelloWorld {
+	private String message;
+	
+	public void setMessage(String msg)
+	{
+		message = msg;
+	}
+	
+	public void getMessage()
+	{
+		System.out.println("Your message is " +message);
+	}
+}
