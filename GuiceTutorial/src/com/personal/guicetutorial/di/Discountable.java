8621d22b4e699639e085ba1d4bb4ffd13e30c366
Sharma <sharashu@amazon.com>
2018-01-07 22:39:30 -0800

    Resolve commit errors

--- GuiceTutorial/src/com/personal/guicetutorial/di/Discountable.java
+++ GuiceTutorial/src/com/personal/guicetutorial/di/Discountable.java
@@ -1,11 +1,8 @@
 package com.personal.guicetutorial.di;
 
-<<<<<<< HEAD
 import com.google.inject.ImplementedBy;
 
 @ImplementedBy(EarlyBirdDiscount.class)
-=======
->>>>>>> d2ccbaa7ac537d499522cc5c7dd64a35bff18d0a
 public interface Discountable {
     double getDiscount();
 }
